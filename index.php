5641427aec138f6369f19de0d73ab23e299845cf
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-09-16 04:09:18 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Check SASSA Status with SassaChecker.co.za</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Check SASSA Status with SassaChecker.co.za</h1>
+        <p>SassaChecker.co.za is your go-to source for checking your SASSA (South African Social Security Agency) status. Whether you're tracking your grant application, eligibility, or payments, we provide you with the tools you need. Stay informed about your SASSA benefits with ease.</p>
+        <p>For more details, visit <a href="https://sassachecker.co.za/">SassaChecker.co.za</a>.</p>
+
+        <div>
+        </div>
+        <a href="https://www.facebook.com/profile.php?id=100089719341976&mibextid=haYZDX" target="_blank"><button>Connect on Facebook</button></a>
+    </div>
+</body>
+
+</html>
